febe397884f7e202bf6a24b3253e99694217423b
arryaaas <aryasalsabila789@gmail.com>
2020-12-18 01:19:19 +0700

    initial commit

---
+++ app.py
@@ -0,0 +1,94 @@
+from flask import Flask, render_template, request
+from scripts import *
+
+app = Flask(__name__)
+
+data_train = data_training()
+data_positif = data_grouping(data_train, "Positif")
+data_negatif = data_grouping(data_train, "Negatif")
+
+@app.route('/', methods=["POST", "GET"])
+def training():
+    title = "Data Training ( Data Latih )"
+    data_train = data_training()
+    return render_template(
+        "training.html", 
+        tables=[data_train.to_html(classes="data", header="true")], title=title
+    )
+
+@app.route('/grouping', methods=["POST", "GET"])
+def grouping():
+    title = "Data Grouping ( Pengelompokan Data )"
+    total_positif = len(data_positif)
+    total_negatif = len(data_negatif)
+    return render_template(
+        "grouping.html", 
+        tables1=[data_positif.to_html(classes="data", header="true")], 
+        tables2=[data_negatif.to_html(classes="data", header="true")], 
+        title=title,
+        total_positif=total_positif,
+        total_negatif=total_negatif
+    )
+
+@app.route('/mean', methods=["POST", "GET"])
+def mean():
+    title = "Mean ( Nilai Rata-rata )"
+    data_mean = df_mean(data_positif, data_negatif)
+    return render_template(
+        "dataframe.html", 
+        tables=[data_mean.to_html(classes="data", header="true")], 
+        title=title
+    )
+
+@app.route('/std', methods=["POST", "GET"])
+def std():
+    title = "Standar Deviasi ( Simpangan Baku )"
+    data_std = df_std(data_positif, data_negatif)
+    return render_template(
+        "dataframe.html", 
+        tables=[data_std.to_html(classes="data", header="true")], 
+        title=title
+    )
+
+@app.route('/prob', methods=["POST", "GET"])
+def prob():
+    title = "Probabilitas ( Peluang )"
+    data_prob = df_prob(data_train, data_positif, data_negatif)
+    return render_template(
+        "dataframe.html", 
+        tables=[data_prob.to_html(classes="data", header="true")], 
+        title=title
+    )
+
+@app.route('/testing')
+def testing():
+    title = "Data Testing ( Data Uji )"
+    return render_template(
+        "testing.html", 
+        title=title
+    )
+
+@app.route('/predict', methods=["POST"])
+def predict():
+    title = "Predict ( Prediksi )"
+
+    df_testing = pd.DataFrame({
+        "Provinsi" : request.form.get("nama_provinsi"),
+        "Dalam Perawatan" : int(request.form.get("dalam_perawatan")),
+        "Sembuh" : int(request.form.get("sembuh")),
+        "Meninggal" : int(request.form.get("meninggal"))
+    }, index = [0])
+
+    data_mean = df_mean(data_positif, data_negatif)
+    data_std = df_std(data_positif, data_negatif)
+
+    classification(df_testing, data_mean, data_std)
+
+    return render_template(
+        "dataframe.html", 
+        tables=[df_testing.to_html(classes="data", header="true")], 
+        title=title
+    )
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
---
+++ scripts/utils.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import math
+# import os
+
+def data_training():
+    # cwd = os.getcwd()
+    # location = cwd + "/data/covid19.csv"
+    # return pd.read_csv(r'%s' % location, sep=';')
+    return pd.read_csv("data/covid19.csv", sep=";")
+
+# df = pd.read_csv("data/covid19.csv", sep=";")
+# print(df)
+
+def data_grouping(df_training, category):
+    return df_training[df_training["Kasus Terbesar Per Provinsi"] == category]
+
+def mean(data):
+    return sum(data) / len(data)
+
+def stdev(data):
+    n = len(data)
+    squares = [item**2 for item in data]
+    variance = ((n*sum(squares)) - (sum(data)**2)) / (n*(n-1))
+    return variance**0.5
+
+def df_mean(df_positif, df_negatif):
+    return pd.DataFrame({
+        "Kasus Terbesar Per Provinsi" : ["Positif", "Negatif"],
+        "Dalam Perawatan" : [mean(df_positif["Dalam Perawatan"]),  mean(df_negatif["Dalam Perawatan"])],
+        "Sembuh" : [mean(df_positif["Sembuh"]), mean(df_negatif["Sembuh"])],
+        "Meninggal" : [mean(df_positif["Meninggal"]), mean(df_negatif["Meninggal"])]
+    })
+
+def df_std(df_positif, df_negatif):
+    return pd.DataFrame({
+        "Kasus Terbesar Per Provinsi" : ["Positif", "Negatif"],
+        "Dalam Perawatan" : [stdev(df_positif["Dalam Perawatan"]), stdev(df_negatif["Dalam Perawatan"])],
+        "Sembuh" : [stdev(df_positif["Sembuh"]), stdev(df_negatif["Sembuh"])],
+        "Meninggal" : [stdev(df_positif["Meninggal"]), stdev(df_negatif["Meninggal"])]
+    })
+
+def df_prob(df_training, df_positif, df_negatif):
+    return pd.DataFrame({
+        "Kasus Terbesar Per Provinsi" : ["Positif", "Negatif"],
+        "Nilai Probabilitas" : [len(df_positif) / len(df_training), len(df_negatif) / len(df_training)]
+    })
+
+def densitas_gauss(x, mean, std):
+    return (1 / math.sqrt(2 * 3.14 * std)) * math.exp(-1 * (((x - mean) ** 2) / (2 * std ** 2)))
+
+def classification(data, df_mean, df_std):
+    category = []
+    
+    for i in range(len(data)):
+        densitas_gauss_positif = [
+            densitas_gauss(data.iloc[i][1], df_mean.iloc[0][1], df_std.iloc[0][1]),
+            densitas_gauss(data.iloc[i][2], df_mean.iloc[0][2], df_std.iloc[0][2]),
+            densitas_gauss(data.iloc[i][3], df_mean.iloc[0][3], df_std.iloc[0][3])
+        ]
+        
+        densitas_gauss_negatif = [
+            densitas_gauss(data.iloc[i][1], df_mean.iloc[1][1], df_std.iloc[1][1]),
+            densitas_gauss(data.iloc[i][2], df_mean.iloc[1][2], df_std.iloc[1][2]),
+            densitas_gauss(data.iloc[i][3], df_mean.iloc[1][3], df_std.iloc[1][3])
+        ]
+
+        posterior = [
+            densitas_gauss_positif[0] * densitas_gauss_positif[1] * densitas_gauss_positif[2],
+            densitas_gauss_negatif[0] * densitas_gauss_negatif[1] * densitas_gauss_negatif[2]
+        ]
+        
+        if max(posterior) == posterior[0]:
+            category.append("Positif")
+        else:
+            category.append("Negatif")
+    
+    data["Kasus Terbesar Per Provinsi"] = category
